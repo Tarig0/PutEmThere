14e40024840c6629a758ab169371869020182b95
Tarig0 <alcook16@gmail.com>
2014-04-12 18:45:37 -0400

    Compress into one file

--- src/net/slimevoid/PutEmThere/CoreLib.java
+++
@@ -1,8 +0,0 @@
-package net.slimevoid.PutEmThere;
-public class CoreLib {
-
-	public static final String MOD_ID = "PutEmThere";
-	public static final String MOD_NAME = "PutEmThere";
-	public static final String MOD_VERSION = "1.0.0";
-
-}
--- src/net/slimevoid/PutEmThere/PutEmThere.java
+++ src/net/slimevoid/PutEmThere/PutEmThere.java
@@ -1,9 +1,16 @@
 package net.slimevoid.PutEmThere;
+
 import java.io.File;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.dedicated.DedicatedServer;
 import net.minecraft.util.ChunkCoordinates;
 import net.minecraftforge.common.Configuration;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -11,10 +18,10 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
-@Mod(modid = CoreLib.MOD_ID, name = CoreLib.MOD_NAME, version = CoreLib.MOD_VERSION)
+@Mod(modid = "PutEmThere", name = "PutEmThere", version = "1.0.0")
 @NetworkMod(clientSideRequired = false, serverSideRequired = false)
 public class PutEmThere {
-	@Instance(CoreLib.MOD_ID)
+	@Instance("PutEmThere")
 	public static PutEmThere instance;
 	private static File configurationFile;
 	private static Configuration configuration;
@@ -44,5 +51,42 @@ public class PutEmThere {
 		z = configuration.get("InitialSpawnPoint", "z", 0).getInt();
 		initialSpawn = new ChunkCoordinates(x, y, z);
 		configuration.save();
+	}
+
+	public class ServerJoinEvent {
+		@ForgeSubscribe
+		public void joinWorld(EntityJoinWorldEvent event) {
+			if (event.entity instanceof EntityPlayerMP) {
+				EntityPlayer player = (EntityPlayerMP) event.entity;
+				File worldDirectory = null;
+				if (FMLCommonHandler.instance().getMinecraftServerInstance() instanceof DedicatedServer) {
+					worldDirectory = FMLCommonHandler
+							.instance()
+							.getMinecraftServerInstance()
+							.getFile(
+									FMLCommonHandler.instance()
+											.getMinecraftServerInstance()
+											.getFolderName());
+				} else {
+					worldDirectory = new File(FMLCommonHandler.instance()
+							.getMinecraftServerInstance().getFile("saves"),
+							FMLCommonHandler.instance()
+									.getMinecraftServerInstance()
+									.getFolderName());
+				}
+
+				File playersDirectory = new File(worldDirectory, "players");
+				File playerFile = new File(playersDirectory,
+						player.getCommandSenderName() + ".dat");
+				if (!playerFile.exists()) {
+					System.out.println("!!!!New Player!!!! PutEmThere");
+					player.setPosition(PutEmThere.initialSpawn.posX,
+							PutEmThere.initialSpawn.posY,
+							PutEmThere.initialSpawn.posZ);
+					player.setSpawnChunk(PutEmThere.initialSpawn, true);
+				}
+			}
+
+		}
 	}
 }
